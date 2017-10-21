2107418b00a302c3cf02e01ec7a1df5ea401720f
Kabir Cahill <kcahill4@my.bcit.ca>
2017-10-20 19:06:16 -0700

    Fixed an error that occurred when updating an item.

--- application/views/itemedit.php
+++ application/views/itemedit.php
@@ -1,10 +1,10 @@
 <h1>Task # {id}</h1>
 <form role="form" action="/mtce/submit" method="post">
     {ftask}
-    {fpriority}
+    {fpriority}<br>
     {zsubmit}
 </form>
 {error}
 
 <a href="/mtce/cancel"><input type="button" value="Cancel the current edit"/></a>
-<a href="/mtce/delete"><input type="button" value="Delete the current edit"/></a>
+<a href="/mtce/delete"><input type="button" value="Delete this todo item"/></a>
