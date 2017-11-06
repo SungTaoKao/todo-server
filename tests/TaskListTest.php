efd3c6dd51b382722ff09ad01cfa76f40e2f4819
SungTaoKao <paulkao94@gmail.com>
2017-11-06 11:50:03 -0800

    Synch with team repo

--- tests/TaskListTest.php
+++ tests/TaskListTest.php
@@ -2,6 +2,10 @@
 
 require_once '../application/models/Tasks.php';
 
+if (! class_exists('PHPUnit_Framework_TestCase')) {
+    class_alias('PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
+}
+
 class TaskListTest extends PHPUnit_Framework_TestCase
 {
     private $tasks;
